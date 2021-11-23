e7437be8b16a8a0a6a35b13cf843d594f133f885
Marc <mark.m@eivolo.com>
2021-11-23 18:50:57 +0200

    Added dynamic element to career

--- vc-elements/elements/PARCareerPositions/PARCareerPositions.php
+++ vc-elements/elements/PARCareerPositions/PARCareerPositions.php
@@ -18,11 +18,48 @@ if (!class_exists('PARCareerPositions')) {
 				'description' => __('', 'parents'),
 				'category' => __('Parents Elements', 'parents'),
 				'params' => array(
-					array(
-						'heading' => 'Text',
-						'type' => 'textfield',
-						'param_name' => 'button',
-					)
+                    array(
+                        'heading' => 'Main Title',
+                        'type' => 'textfield',
+                        'param_name' => 'main_title',
+                    ),
+                    array(
+                        'heading' => 'Counter Title and Text',
+                        'type' => 'param_group',
+                        'param_name' => 'informations',
+                        'params' => array(
+                            array(
+                                'param_name' => "title",
+                                'type' => 'textfield',
+                                'heading' => "Title"
+                            ),
+                            array(
+                                'param_name' => "location",
+                                'type' => 'textfield',
+                                'heading' => "Location",
+                            ),
+                            array(
+                                'param_name' => "time",
+                                'type' => 'textfield',
+                                'heading' => "Time",
+                            ),
+                            array(
+                                'param_name' => "position",
+                                'type' => 'textfield',
+                                'heading' => "Position",
+                            ),
+                            array(
+                                'param_name' => "button_title",
+                                'type' => 'textfield',
+                                'heading' => "Button Title",
+                            ),
+                            array(
+                                'param_name' => "button_url",
+                                'type' => 'textfield',
+                                'heading' => "Button URL",
+                            ),
+                        ),
+                    ),
 				),
 			));
 		}
@@ -36,8 +73,11 @@ if (!class_exists('PARCareerPositions')) {
 			wp_enqueue_script('par_career_positions-script', get_template_directory_uri() .
 				"/vc-elements/elements/PARCareerPositions/twig-templates/par_career_positions.js", array('jquery'), '1.0', true);
 
-
-			return $this->twigObj->render("par_career_positions.html.twig", array());
+            $counters = vc_param_group_parse_atts($atts['informations']);
+			return $this->twigObj->render("par_career_positions.html.twig", array(
+                'informations' => $counters,
+                'main_title' => $atts['main_title']
+            ));
 		}
 	}
 }
