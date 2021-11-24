6657a19d3f204df97bd988c69bc08e2c4bb7169e
Ayman Abi Aoun <ayman@abiroot.com>
2021-11-24 13:59:16 +0200

    Style fix

--- functions.php
+++ functions.php
@@ -9,7 +9,7 @@
 
 if ( ! defined( 'PARENTS_VERSION' ) ) {
 	// Replace the version number of the theme on each release.
-	define( 'PARENTS_VERSION', '1.2.2' );
+	define( 'PARENTS_VERSION', '1.2.3' );
 }
 
 if ( ! function_exists( 'parents_setup' ) ) :
