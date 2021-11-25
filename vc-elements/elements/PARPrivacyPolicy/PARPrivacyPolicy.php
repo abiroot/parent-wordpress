8d33fe95a8575bccb8effb906bb4f91ee15205ea
Ayman Abi Aoun <ayman@abiroot.com>
2021-11-25 03:22:22 +0200

    Fixed privacy policy

--- vc-elements/elements/PARPrivacyPolicy/PARPrivacyPolicy.php
+++ vc-elements/elements/PARPrivacyPolicy/PARPrivacyPolicy.php
@@ -69,7 +69,9 @@ if (!class_exists('PARPrivacyPolicy')) {
 			$pages = vc_param_group_parse_atts($atts['policy_pages']);
 			array_walk($pages, function (&$item, $va) {
 				if (!empty($item['main_content'])) {
-					$item['main_content'] = base64_decode($item['main_content']);
+					if(isBase64Encoded($item['main_content'])) {
+						$item['main_content'] = base64_decode($item['main_content']);
+					}
 				}
 				$item['main_image'] = wp_get_attachment_image_src($item['main_image'], 'medium');
 				$item['main_image'] = $item['main_image'][0];
