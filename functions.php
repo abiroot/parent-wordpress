4b8816b1022b47e1bb202d80fd21ab88b1a0ace8
Ayman Abi Aoun <ayman@abiroot.com>
2021-11-25 19:32:03 +0200

    Increment Version

--- functions.php
+++ functions.php
@@ -9,7 +9,7 @@
 
 if ( ! defined( 'PARENTS_VERSION' ) ) {
 	// Replace the version number of the theme on each release.
-	define( 'PARENTS_VERSION', '1.3.0' );
+	define( 'PARENTS_VERSION', '1.3.1' );
 }
 
 if ( ! function_exists( 'parents_setup' ) ) :
