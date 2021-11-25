38d2f3dc940f8b71307d3edb13912f5a5b83e227
Ayman Abi Aoun <ayman@abiroot.com>
2021-11-25 19:29:22 +0200

    Fix Menu

--- header.php
+++ header.php
@@ -38,8 +38,8 @@
 					the_custom_logo();
 					?>
                 </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample07"
-                        aria-controls="navbarsExample07" aria-expanded="false" aria-label="Toggle navigation">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mainNavbar"
+                        aria-controls="mainNavbar" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
 
